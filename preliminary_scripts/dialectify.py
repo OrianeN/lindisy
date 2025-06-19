ec6bf52c69966a3932b4d22777fcb22bf0b0646d
OrianeN <oriane.nedey@inria.fr>
2025-06-19 15:37:13 +0200

    Refactor + Make intermediary corpus before generating varieties from it

--- preliminary_scripts/dialectify.py
+++ preliminary_scripts/dialectify.py
@@ -16,14 +16,56 @@ from gliner import GLiNER
 
 from rule_change_applier import RuleChangeApplier
 
-diacritics = {  # Found at https://www.lfsag.unito.it/ipa/index_fr.html
-    " ̥", " ̊", "̬", "ʰ", "̹", "̜", "̟", "̠", " ̈", "̽",
-    "̩", "̯", "˞", "̤", "̰", "̼", "ʷ", "ʲ", "ˠ", "ˤ", "̴",
-    "̝", "̞", "̘", "̙", "̪", "̺", "̻", " ̃", "ⁿ", "ˡ", "̚",
-    "͡"  # affricate symbol
-}
 
-PH_TEXT = "PH"
+def load_sound_rules(path_sound_rules: str):
+    # Load phonetic transformation rules
+    with open(path_sound_rules) as f:
+        dialects_dict = json.load(f)
+        rules_dict = load_dialect_rules(dialects_dict)
+    for dialect_id, dialect_rules in rules_dict.items():
+        rca = RuleChangeApplier.from_list(dialect_rules)
+        rules_dict[dialect_id] = rca
+    return rules_dict
+
+
+def load_dialect_rules(dialects_tree, parent_rules=[]):
+    dialect2rules = {}
+    for node in dialects_tree:
+        # Inherit parent rules
+        node_rules = parent_rules.copy()
+        node_rules.extend(node["rules"])
+
+        if "children" not in node:  # Dialect leaf = end of branch
+            dialect2rules[node["id"]] = node_rules
+        else:  # (sub)family of dialects
+            # Recurse for children nodes/dialects
+            dialect2rules.update(load_dialect_rules(
+                node["children"],
+                parent_rules=node_rules
+            ))
+
+    return dialect2rules
+
+
+class Phonetizer:
+    DIACRITICS = {  # Found at https://www.lfsag.unito.it/ipa/index_fr.html
+        " ̥", " ̊", "̬", "ʰ", "̹", "̜", "̟", "̠", " ̈", "̽",
+        "̩", "̯", "˞", "̤", "̰", "̼", "ʷ", "ʲ", "ˠ", "ˤ", "̴",
+        "̝", "̞", "̘", "̙", "̪", "̺", "̻", " ̃", "ⁿ", "ˡ", "̚",
+        "͡"  # affricate symbol
+    }
+
+    def __init__(self, lang="deu-Latn"):
+        self.epi = epitran.Epitran(lang)
+
+    def __call__(self, word):
+        transcription = self.epi.transliterate(word)
+        # Normalize/Simplify diacritics (in phonetic transcription)
+        transcription = unicodedata.normalize("NFKD", transcription)
+        found_diacritics = self.DIACRITICS.intersection(set(transcription))
+        if found_diacritics:
+            transcription = re.sub(rf"[{''.join(found_diacritics)}]", "", transcription)
+        return transcription
 
 
 class Graphemizer:
@@ -66,6 +108,8 @@ class Graphemizer:
 
 
 class NER:
+    PH_TEXT = "PH"  # TODO Use pseudo-XML instead (<ph>entity</ph>)
+
     def __init__(self, model_name="urchade/gliner_multi-v2.1"):
         self.model = GLiNER.from_pretrained(model_name)
         self.labels = ["named entity", "name", "organization", "work title", "event"]  # TODO Add "web address" (url, email...) ?
@@ -90,64 +134,112 @@ class NER:
         return text_masked
 
 
-def load_dialect_rules(dialects_tree, parent_rules=[]):
-    dialect2rules = {}
-    for node in dialects_tree:
-        # Inherit parent rules
-        node_rules = parent_rules.copy()
-        node_rules.extend(node["rules"])
+class Dialectifier:
 
-        if "children" not in node:  # Dialect leaf = end of branch
-            dialect2rules[node["id"]] = node_rules
-        else:  # (sub)family of dialects
-            # Recurse for children nodes/dialects
-            dialect2rules.update(load_dialect_rules(
-                node["children"],
-                parent_rules=node_rules
-            ))
+    def __init__(self, path_sound_rules: str, path_p2g: str, path_lexicon: str,
+                 output_folder: str, corpus_name: str, lang="deu-Latn"):
+        self.sound_rules = load_sound_rules(path_sound_rules)
 
-    return dialect2rules
+        # Load external tools: compound splitter, NER model, phonetizer, graphemizer
+        self.ner = NER()
+        self.phonetizer = Phonetizer(lang)
+        self.graphemizer = Graphemizer(path_p2g)
+        self.ahocs = comp_split.read_dictionary_from_file(path_lexicon)  # ahocs stands for Aho-Corasick search method - used by the compound splitter
 
+        # Setup paths
+        self.output_folder = output_folder
+        self.corpus_name = corpus_name
+        self.corpus_prep_path = None
 
-def tokenize(line):
-    return re.split(r"([[:^alpha:]]+)", line)  # TODO Add '+' to get groups of non-alpha characters ?
+    def preprocess_corpus(self, corpus_path: str):
+        self.corpus_prep_path = self.make_output_path("preprocessed")
+        with open(corpus_path) as f_in, open(self.corpus_prep_path, "w") as f_out:
+            for line in f_in:
+                line_units = []
+                # Detect and mask named entities
+                line = self.ner.mask_entities(line)
+                # Tokenize
+                units = self.tokenize(line)
+                for unit in units:
+                    # Phonetize+graphemize words (but keep punctuation and whitespaces as is)
+                    if self.word_needs_transformation(unit):
+                        # TODO Count number of words phonetized + filter if <3
+                        # TODO Detect type of capitalization
+                        # Split if compound word
+                        unit_splits = self.split_compound(unit)
+                        mod_splits = []
+                        for split in unit_splits:
+                            split = self.phonetizer(split)
+                            mod_splits.append(split)
+                        line_units.append(("alpha", mod_splits))
+                    else:
+                        line_units.append(("copy", unit))
+                # Write transcribed line to output file
+                f_out.write(json.dumps(line_units)+"\n")
 
+    def generate_variety_corpus(self, dialect_id: [str, None], out_path: str):
+        with open(self.corpus_prep_path) as f_prep, open(out_path, "w") as f_out:
+            for line in f_prep:
+                transcribed_line = ""
+                units = json.loads(line.strip())
+                for unit_type, unit_val in units:
+                    if unit_type == "alpha":
+                        assert isinstance(unit_val, list)
+                        merged_splits = ""
+                        for split in unit_val:
+                            if dialect_id is not None:
+                                # Apply dialect sound change rules
+                                split = self.sound_rules[dialect_id].apply_changes(split)
+                            split = self.graphemizer.apply_rules(split)
+                            merged_splits += split
+                        unit_val = merged_splits
+                    transcribed_line += unit_val
+                # Write transcribed line to output file
+                f_out.write(transcribed_line)
 
-def word_needs_transformation(word):
-    if word == PH_TEXT:
-        return False
-    else:
-        return bool(re.match(r"[[:alpha:]]", word))
+    def generate_all_varieties(self):
+        # Generate corpus for standard variety
+        out_path = self.make_output_path("standard")
+        print(f"Generate corpus for the standard variety (to {out_path})")
+        self.generate_variety_corpus(dialect_id=None, out_path=out_path)
 
+        # Generate all dialects
+        for dialect_id in self.sound_rules.keys():
+            print(f"Generate corpus for dialect {dialect_id} (to {out_path})")
+            out_path = self.make_output_path(f"dialect-{dialect_id}")
+            self.generate_variety_corpus(dialect_id, out_path)
 
-def split_compound(word, ahocs):
-    """
-    Split and mark compound word.
-    :param word: str. word to attempt to split
-    :param ahocs: loaded lexicon (ahocs stands for Aho-Corasick search method)
-    :return: list of word parts
-    """
-    try:
-        # TODO Silence prints (temporary mockup function ?)
-        dissection = comp_split.dissect(word, ahocs, only_nouns=False)  # TODO Decide only_nouns value (True tends to oversplit)
-    except IndexError:  # There seems to be a bug with the implementation which raises an error when a word is not found
-        return [word]
+    def make_output_path(self, suffix):
+        return os.path.join(self.output_folder, f"{self.corpus_name}-{suffix}.txt")
 
-    if len(dissection) <= 1:
-        return [word]
-    else:
-        return dissection
+    @staticmethod
+    def tokenize(line):
+        return re.split(r"([[:^alpha:]]+)", line)  # TODO Add '+' to get groups of non-alpha characters ?
 
+    @staticmethod
+    def word_needs_transformation(word):
+        if word == NER.PH_TEXT:
+            return False
+        else:
+            return bool(re.match(r"[[:alpha:]]", word))
 
-def phonetize_word(word, phonetizer):
-    # Phonetize
-    transcription = phonetizer.transliterate(word)
-    # Normalize/Simplify diacritics (in phonetic transcription)
-    transcription = unicodedata.normalize("NFKD", transcription)
-    found_diacritics = diacritics.intersection(set(transcription))
-    if found_diacritics:
-        transcription = re.sub(rf"[{''.join(found_diacritics)}]", "", transcription)
-    return transcription
+    def split_compound(self, word):
+        """
+        Split and mark compound word.
+        :param word: str. word to attempt to split
+        :param ahocs: loaded lexicon (ahocs stands for Aho-Corasick search method)
+        :return: list of word parts
+        """
+        try:
+            # TODO Silence prints (temporary mockup function ?)
+            dissection = comp_split.dissect(word, self.ahocs, only_nouns=False)  # TODO Decide only_nouns value (True tends to oversplit)
+        except IndexError:  # An error is raised when a word is not found in the lexicon
+            return [word]
+
+        if len(dissection) <= 1:
+            return [word]
+        else:
+            return dissection
 
 
 if __name__ == "__main__":
@@ -164,80 +256,25 @@ if __name__ == "__main__":
 
     args = argparser.parse_args()
 
-    # Load phonetic transformation rules
-    with open(args.rules_phonetic) as f:
-        dialects_dict = json.load(f)
-        rules_dict = load_dialect_rules(dialects_dict)
-    for dialect_id, dialect_rules in rules_dict.items():
-        rca = RuleChangeApplier.from_list(dialect_rules)
-        rules_dict[dialect_id] = rca
-
     # Setup file/folder names
     output_folder = args.output
-    os.makedirs(output_folder)
+    os.makedirs(output_folder)  # TODO Auto-increment ?
     corpus_name = os.path.splitext(os.path.basename(args.corpus))[0]
 
-    # Load external tools: compound splitter, NER model, phonetizer, graphemizer
-    ner = NER()
-    phonetizer = epitran.Epitran(args.lang)
-    graphemizer = Graphemizer(args.rules_p2g)
-    ahocs = comp_split.read_dictionary_from_file(args.lexicon)  # ahocs stands for Aho-Corasick search method - used by the compound splitter
+    # Initialize Dialectifier
+    dialectifier = Dialectifier(
+        path_sound_rules=args.rules_phonetic,
+        path_p2g=args.rules_p2g,
+        path_lexicon=args.lexicon,
+        output_folder=output_folder,
+        corpus_name=corpus_name,
+        lang=args.lang
+    )
 
-    # Phonetize input corpus  # TODO Refactor to go through input corpus only once
-    print(f"Generate phonetic transcription of the input corpus (standard)")
-    out_path = os.path.join(output_folder, f"{corpus_name}-standard.txt")
-    with open(args.corpus) as f_in, open(out_path, "w") as f_out:
-        for line in f_in:
-            # Detect and mask named entities
-            line = ner.mask_entities(line)
-            # Tokenize
-            units = tokenize(line)
-            transcribed_line = ""
-            for unit in units:
-                # Phonetize+graphemize words (but keep punctuation and whitespaces as is)
-                if word_needs_transformation(unit):
-                    # TODO Count number of words phonetized + filter if <3
-                    # Split if compound word
-                    unit_splits = split_compound(unit, ahocs)
-                    mod_unit = ""
-                    for split in unit_splits:
-                        split = phonetize_word(split, phonetizer)  # TODO Phonetize only if word is in lexicon ?
-                        split = graphemizer.apply_rules(split)
-                        mod_unit += split
-                    unit = mod_unit
-                transcribed_line += unit
-            # Write transcribed line to output file
-            f_out.write(transcribed_line)
+    # Preprocess corpus
+    dialectifier.preprocess_corpus(args.corpus)
 
-    # Dialectify corpus line by line for each dialect
-    for dialect_id in rules_dict.keys():
-        print(f"Generate corpus for dialect {dialect_id} (to {out_path})")
-        out_path = os.path.join(output_folder, f"{corpus_name}-dialect-{dialect_id}.txt")
-        with open(args.corpus) as f_in, open(out_path, "w") as f_out:
-            for line in f_in:
-                # Detect and mask named entities
-                line = ner.mask_entities(line)
-                # Tokenize line on all whitespace and punctuation
-                units = tokenize(line)
-                # Dialectify each unit (token, whitespace, punctuation...)
-                dialectified_str = ""
-                for unit in units:
-                    if word_needs_transformation(unit):
-                        # Split if compound word
-                        unit_splits = split_compound(unit, ahocs)
-                        dialect_unit = ""
-                        for split in unit_splits:
-                            # Phonetize
-                            split = phonetize_word(split, phonetizer)
-                            # Apply dialect sound change rule
-                            split = rules_dict[dialect_id].apply_changes(split)
-                            # Graphemize
-                            split = graphemizer.apply_rules(split)
-                            dialect_unit += split
-                    else:
-                        dialect_unit = unit
-                    dialectified_str += dialect_unit
-                # Write dialectified line to output file
-                f_out.write(dialectified_str)
+    # Generate corpora for all varieties (incl. standard)
+    dialectifier.generate_all_varieties()
 
     print(f"Done !")
